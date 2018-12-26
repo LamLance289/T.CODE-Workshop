4165c0515d8039aef1023da51ac494840c0a47e9
LamLance289 <33772152+LamLance289@users.noreply.github.com>
2018-12-26 00:35:31 -0800

    Add files via upload
    Bai thu 4

---
+++ Assets/Script/VelocityController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb)
+        {
+            rb.velocity = Vector3.right;
+            
+        }
+    }
+}
+	
+	
+	
