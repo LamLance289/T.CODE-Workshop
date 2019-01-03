762a983d3f67ac0e6958407a1b19196404fa2be0
LamLance289 <lam.lance289@gmail.com>
2019-01-02 22:55:52 -0800

    no message

---
+++ Chrome Rex/Assets/Script/DinoController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoController : MonoBehaviour
+{
+
+    public bool isGrounded;
+    Rigidbody rb;
+    bool isgrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionStay()
+    {
+        isgrounded = true;
+    }
+    void Update()
+    {
+        //Jump if pressing space
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 5, 0),
+           ForceMode.Impulse);
+            isgrounded = false;
+
+        }
+    }
+}
+    
+
+
---
+++ Chrome Rex/Assets/Script/GrassScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassScroll : MonoBehaviour {
+
+    public float Speed = 0.2f;
+    private MeshRenderer render;
+    private Vector2 offsetDefault;
+
+    void Start()
+    {
+        render = GetComponent<MeshRenderer>();
+        offsetDefault = render.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * Speed, 1);
+        Vector2 newOffset = new Vector2(x, offsetDefault.y);
+        render.sharedMaterial.SetTextureOffset("_MainTex", newOffset);
+    }
+}
